17f65233d579b63445664a27dfa0199f71ab4bf4
Nurdoolot <nalymbaev77@gmail.com>
2025-07-05 13:34:43 +0600

    Created classes Patient, Main, Appointment and AppointmentManager

---
+++ Patient.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+
+public class Patient {
+    private final String fullName;
+    private final LocalDate birthDate;
+    private final String type; // "первичный" или "вторичный"
+    private final String symptoms;
+
+    public Patient(String fullName, LocalDate birthDate, String type, String symptoms) {
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.type = type;
+        this.symptoms = symptoms;
+    }
+
+    public String getFullName() { return fullName; }
+    public LocalDate getBirthDate() { return birthDate; }
+    public String getType() { return type; }
+    public String getSymptoms() { return symptoms; }
+}
